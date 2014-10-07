c05d2e4c20d3cdaad2d40cd5e4f26c91b21525b0
JaxKodex <jorge12745@hotmail.com>
2014-10-06 22:20:36 -0500

    Menu Style

--- db/init.sql
+++ db/init.sql
@@ -1,2 +1,6 @@
 use edustats;
 insert into rol values (null, 'ROL_USER');
+insert into institucion_educativa values (null, 'Santa Rosa del Sauce', 'IESRS');
+insert into persona values ('Jax', '', 'kodex', '', '12345678');
+insert into usuario values ('jaxkodex', md5('123456'), 1, '12345678');
+insert into usuario_has_rol values ('jaxkodex', 1);
