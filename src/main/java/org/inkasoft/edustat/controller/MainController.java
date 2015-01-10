f446b4d7ebdfb9577d589f8152ba3ee447efb1a2
jaxkodex <jvilcay@gmail.com>
2015-01-10 11:00:00 -0500

    merge

--- src/main/java/org/inkasoft/edustat/controller/MainController.java
+++ src/main/java/org/inkasoft/edustat/controller/MainController.java
@@ -1,13 +1,18 @@
 package org.inkasoft.edustat.controller;
 
+import java.util.Date;
+
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+	public Logger logger = Logger.getLogger(MainController.class);
 
 	@RequestMapping(value={"/", "index.do"})
 	public String index () {
+		logger.debug("Entrando a Index "+new Date());
 		return "index";
 	}
 
